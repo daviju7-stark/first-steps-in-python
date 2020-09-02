765868aa60212e7f28a70e31520cb3614b83235a
DanyBoy <danielvillarrealj15@gmail.com>
2020-09-02 16:56:56 -0500

    second program

---
+++ program2.py
@@ -0,0 +1,18 @@
+cost_of_dinner = float(input("Cost of Dinner:\n $"))
+servece = cost_of_dinner * 6.2 / 100.0 
+tip = cost_of_dinner * 10.0 / 100.0
+Return = cost_of_dinner + servece + tip
+print("Total:$", Return)
+
+
+
+
+
+
+
+
+
+
+
+
+
