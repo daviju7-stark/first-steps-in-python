2a4c761065635b05b68bda6c460d45497a82a515
DanyBoy <danielvillarrealj15@gmail.com>
2020-09-02 17:04:25 -0500

    fourth program

---
+++ program4.py
@@ -0,0 +1,7 @@
+capacity = float(input("Fuel Tank Capacity:"))
+kilometres = float(input("Performance (KIlometer per liter):"))
+travel = float(input("Kilometers traveled:"))
+kmXtank = capacity*kilometres
+print("Well be necesary:",travel/kmXtank,"tanks.")
+
+
