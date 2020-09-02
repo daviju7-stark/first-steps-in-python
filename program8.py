250b112615cc2e56419c6fb3642a7d61a469ee47
DanyBoy <danielvillarrealj15@gmail.com>
2020-09-02 17:10:27 -0500

    eighth program

---
+++ program8.py
@@ -0,0 +1,17 @@
+
+vote=input("Enter your candidate: ").capitalize()
+cadidateA="Red"
+cadidateB="Green"
+cadidateC="Blue"
+if vote == cadidateA:
+    print("You have voted for the Red candidate")
+elif vote == cadidateB:
+    print("You have voted for the Green candidate")
+elif vote == cadidateC:
+    print("You have voted for the Blue candidate")
+else:
+    print("Wrong choice")
+    
+
+    
+
