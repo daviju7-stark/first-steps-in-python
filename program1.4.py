c3cf01eef313834984404d138fc5bc7d82413f1a
DanyBoy <danielvillarrealj15@gmail.com>
2020-09-18 08:07:38 -0500

    While program

---
+++ program1.4.py
@@ -0,0 +1,17 @@
+#Pair numbers
+
+pares=0
+n=int(input("Número (-1 para terminar el programa): "))
+while n!=-1:
+    if n%2==0:
+        pares+=1
+    suma=0
+    while n!=0:
+        digito=n%10
+        suma+=digiton
+        n=n//10
+    print("Suma de sus digitos:", suma)
+    n= int(input("Número (-1 para terminar el programa): "))
+print("Se ingresaron", pares, "Números pares")
+
+
